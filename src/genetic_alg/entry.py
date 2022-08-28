4cff5aa9f867deaa059987f1b84ab2b5f6968add
Cardroid <number1dh@gmail.com>
2022-08-28 17:42:54 +0900

    Chore: 프로젝트 설명 수정

--- src/genetic_alg/entry.py
+++ src/genetic_alg/entry.py
@@ -5,10 +5,11 @@ from genetic_alg import log
 def main():
     import argparse
 
-    parser = argparse.ArgumentParser(description="미디어를 압축 인코딩합니다.")
+    parser = argparse.ArgumentParser(description="유전 알고리즘 프로젝트")
 
-    parser.add_argument("-i", dest="input", required=True, help="하나 이상의 입력 소스 파일 및 디렉토리 경로")
+    parser.add_argument("-i", dest="input", required=True, help="데이터 파일 경로")
     parser.add_argument("-o", dest="output", default="out", help="출력 디렉토리 경로")
+    parser.add_argument("-m", dest="mode", default="divide", help="로그 디렉토리 경로")
     parser.add_argument("--log-dir", dest="log_path", default=log.DIR_PATH, help="로그 디렉토리 경로")
     parser.add_argument("--log-level", dest="log_level", default=log.ROOT_LOG_LEVEL, choices=["debug", "info", "warning", "error", "critical"], help="로그 출력 레벨")
     parser.add_argument("--no-graph", dest="use_graph", action="store_false", help="그래프 시각화 사용여부")
